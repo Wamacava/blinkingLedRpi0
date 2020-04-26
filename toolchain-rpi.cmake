ad8b171f8e971f468df58bceee556d3440cd4c7b
lukasz <lukasz.kulas@domino-uk.com>
2020-04-26 17:08:07 +0100

    change compiler check

--- toolchain-rpi.cmake
+++ toolchain-rpi.cmake
@@ -2,13 +2,6 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-# Usually CMake is compiling simple test program and runs it
-# In case of cross compiling the compiled program can't be run on host 
-# so to avoid trying to run compiled program we can check by compiling library
-# other solution is set CMAKE_C_COMPILER_WORKS
-#set(CMAKE_C_COMPILER_WORKS TRUE)
-set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
-
 # Define the cross compiler locations
 SET(CMAKE_C_COMPILER   /home/lukasz/dev/cmakeapp/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER /home/lukasz/dev/cmakeapp/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
